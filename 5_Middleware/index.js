8dec83a4aa60cfaac3f342f4df40d72aa3710eb5
OlegusSPb888 <118619576+OlegusSPb888@users.noreply.github.com>
2023-04-17 00:39:14 +0300

    Add files via upload

---
+++ 5_Middleware/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const apiBook = require('./routes/book');
+const apiUser = require('./routes/user');
+const error404 = require('./middleware/err_404')
+
+const PORT = process.env.PORT || 3000
+
+const app = express();
+
+app.use(express.json());
+app.use('/public', express.static(__dirname + '/public'))
+app.use('/api/user', apiUser)
+app.use('/api', apiBook)
+app.use(error404)
+
+app.listen(PORT)
