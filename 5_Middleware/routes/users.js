12bd5ad20a7bd7ea313a8dae5205a9a6828a981d
OlegusSPb888 <118619576+OlegusSPb888@users.noreply.github.com>
2023-04-17 00:38:10 +0300

    Delete users.js

--- 5_Middleware/routes/users.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-
-router.post("/login", (_req, res) => {
-	res.status(201).json({ id: 1, firstName: "Semen", lastName: "Semenov" });
-});
-
-module.exports = router;
